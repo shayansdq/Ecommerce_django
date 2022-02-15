d977edad6df4751ddbd6c7557ce4e1df25b2dff0
Shayan <shayansadeghi178@gmail.com>
2022-02-16 01:35:11 +0330

    refactor test

--- products/tests/test_model.py
+++ products/tests/test_model.py
@@ -1,10 +1,13 @@
+from django.db.models import Count
+from django.utils import timezone
+
 from orders.models import Cart, CartItem, OffCode
 from customers.models import Customer
 from products.models import Product, Brand, Discount, Category
 from django.test import TestCase
 
 
-class CartTest(TestCase):
+class ProductTest(TestCase):
     def setUp(self) -> None:
         self.customer1 = Customer.objects.create_user('test1', 'test1@email.com', 'test1', gender=1,
                                                       phone_number='09224023292')
@@ -17,7 +20,7 @@ class CartTest(TestCase):
 
         # categories
         self.category1 = Category.objects.create(name='Electrical')
-        self.category2 = Category.objects.create(name='mobil', root=self.category1)
+        self.category2 = Category.objects.create(name='mobile', root=self.category1)
 
         # discounts
         self.discount1 = Discount.objects.create(value=2000, type='PR')
@@ -27,7 +30,7 @@ class CartTest(TestCase):
         self.product1 = Product.objects.create(name='TV', price=100000, description='some text', inventory=5,
                                                brand=self.brand1,
                                                category=self.category1, discount=self.discount1, slug='some-text')
-        self.product2 = Product.objects.create(name='J5', price=200000, description='some text', inventory=10,
+        self.product2 = Product.objects.create(name='J5', price=200, description='some text', inventory=10,
                                                brand=self.brand2,
                                                category=self.category2, slug='another-text')
 
@@ -36,9 +39,8 @@ class CartTest(TestCase):
         self.cart_item1 = CartItem.objects.create(cart=self.cart1, product=self.product1)
         self.cart_item2 = CartItem.objects.create(cart=self.cart1, product=self.product2)
 
-    def test1_total_worth_success(self):
-        print(self.cart1.total_worth())
-
-    def test2_final_worth_success(self):
-        print(self.cart1.final_worth())
-
+    def test1_find_max_price_product(self):
+        # self.assertEqual(Product.max_price(),Product.objects.all().values())
+        max_price = max(Product.objects.all().values_list('price', flat=True))
+        self.assertEqual(Product.max_price().price, max_price)
+        print(Product.objects.annotate(Count('brand')))
