9dce92b0636fc039118fc9a95ad5e8fe33e82094
Shayan <shayansadeghi178@gmail.com>
2022-02-15 15:29:54 +0330

    customizing address and customer models > admin.py

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Customer, Address
 
-# Register your models here.
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    extra = 1
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email',)
+    search_fields = ('username', 'email',)
+    inlines = [AddressInline]
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('state', 'city', 'customer')
+    search_fields = ('state', 'city', 'customer')
+    list_filter = ('last_updated',)
