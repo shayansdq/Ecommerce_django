d0f7428246a50882febb6ed999c37202a185edb4
Shayan <shayansadeghi178@gmail.com>
2022-02-15 16:39:14 +0330

    customizing orders app models for admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Cart, OffCode, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'final_price',)
+    search_fields = ('customer',)
+    list_filter = ('created',)
+    raw_id_fields = ('customer', 'off_code')
+
+
+@admin.register(OffCode)
+class OffCodeAdmin(admin.ModelAdmin):
+    list_display = ('value', 'type',)
+    search_fields = ('value', 'type')
+    list_filter = ('created',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'count',)
+    search_fields = ('product', )
+    list_filter = ('cart',)
+    raw_id_fields = ('cart', 'product')
