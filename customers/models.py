1d2a38890e7117d367a47d8244dffef2900330d7
Shayan <shayansadeghi178@gmail.com>
2022-02-15 15:29:05 +0330

    create address and customer models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from core.models import BaseModel
 
-# Create your models here.
+
+class Customer(User):
+    """
+        A class used to implement customers
+    """
+    # first_name = models.CharField(max_length=32, verbose_name='First Name')
+    # last_name = models.CharField(max_length=32, verbose_name='Last Name')
+    # email = models.EmailField(verbose_name='Email')
+    phone_number = models.CharField(max_length=32, verbose_name='Phone Number')
+    gender = models.IntegerField(choices=[
+        (1, 'Male'),
+        (2, 'Female'),
+        (3, 'Other'),
+    ])
+
+
+class Address(BaseModel):
+    """
+        A class used to implement customers
+    """
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.IntegerField(max_length=10,verbose_name='Postal Code')
+    extra_detail = models.TextField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses')
