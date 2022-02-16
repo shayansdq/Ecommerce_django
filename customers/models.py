1ff6aa3dc38f3554361c96c5218f0a392bb3b173
Shayan <shayansadeghi178@gmail.com>
2022-02-16 14:21:36 +0330

    refactor & update translation for verbose names

--- customers/models.py
+++ customers/models.py
@@ -11,30 +11,31 @@ class Customer(User):
     # first_name = models.CharField(max_length=32, verbose_name='First Name')
     # last_name = models.CharField(max_length=32, verbose_name='Last Name')
     # email = models.EmailField(verbose_name='Email')
-    phone_number = models.CharField(max_length=32, verbose_name='Phone Number')
+    phone_number = models.CharField(max_length=32, verbose_name=_('Phone Number'))
     gender = models.IntegerField(choices=[
         (1, _('Male')),
-        (2, 'Female'),
-        (3, 'Other'),
-    ],verbose_name='Gender')
+        (2, _('Female')),
+        (3, _('Other')),
+    ],verbose_name=_('Gender'))
 
     class Meta:
-        verbose_name = 'Customer'
-        verbose_name_plural = 'Customers'
+        verbose_name = _('Customer')
+        verbose_name_plural = _('Customers')
 
 
 class Address(BaseModel):
     """
         A class used to implement customers
     """
-    state = models.CharField(max_length=50, verbose_name='State')
-    city = models.CharField(max_length=50, verbose_name='City')
-    zip_code = models.PositiveIntegerField(unique=True,verbose_name='Zip Code')
-    extra_detail = models.TextField(verbose_name='Address detail')
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses',verbose_name='Customer')
+    state = models.CharField(max_length=50, verbose_name=_('State'))
+    city = models.CharField(max_length=50, verbose_name=_('City'))
+    zip_code = models.PositiveIntegerField(unique=True,verbose_name=_('Zip Code'))
+    extra_detail = models.TextField(verbose_name=_('Address detail'))
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses',
+                                 verbose_name=_('Customer'))
 
     class Meta:
         index_together = ["city", "zip_code"]
         unique_together = ["city", "zip_code"]
-        verbose_name = 'Address'
-        verbose_name_plural = 'Addresses'
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
