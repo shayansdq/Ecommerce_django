9c124c0d87309e6bfecd542a9b7df05d7ab46649
Shayan <shayansadeghi178@gmail.com>
2022-02-16 14:22:12 +0330

    config initial configs for statics and media

--- Ecommerce_django/settings.py
+++ Ecommerce_django/settings.py
@@ -108,7 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+LANGUAGES = (
+    ('en', 'English'),
+    ('fa', 'Farsi')
+)
+LOCALE_PATHS = (BASE_DIR / 'locale/',)
+
 
 TIME_ZONE = 'UTC'
 
@@ -121,6 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# Media Configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
