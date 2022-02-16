00d09055ef976d10fbb1c142560ebfe88f79386c
Shayan <shayansadeghi178@gmail.com>
2022-02-16 14:21:09 +0330

    add cartitem inline model

--- orders/admin.py
+++ orders/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from .models import Cart, OffCode, CartItem
 
 
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 2
+
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('customer', 'final_price',)
     search_fields = ('customer',)
     list_filter = ('created',)
     raw_id_fields = ('customer', 'off_code')
+    inlines = (CartItemInline,)
 
 
 @admin.register(OffCode)
@@ -20,6 +26,6 @@ class OffCodeAdmin(admin.ModelAdmin):
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('product', 'count',)
-    search_fields = ('product', )
+    search_fields = ('product',)
     list_filter = ('cart',)
     raw_id_fields = ('cart', 'product')
