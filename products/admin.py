e53cbbfb0660d96127f0019ef205a24779296b2f
Shayan <shayansadeghi178@gmail.com>
2022-02-16 16:40:34 +0330

    add some features to models

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Product, Brand, Category, Discount
+from .models import Product, Brand, Category, Discount, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'category')
     search_fields = ('name', 'category', 'brand')
+    list_display_links = ('name',)
+    list_per_page = 20
     list_filter = ('inventory',)
     raw_id_fields = ('brand', 'discount', 'category')
     fieldsets = (
@@ -14,15 +16,34 @@ class ProductAdmin(admin.ModelAdmin):
         ('Specifications',
          {'fields': ['discount', 'inventory', 'slug', 'picture']})
     )
-    prepopulated_fields = {'slug': ('description',)}
+    prepopulated_fields = {'slug': ('name',)}
+    exclude = ('delete_timestamp', 'deleted_at', 'is_deleted', 'is_active')
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'country')
     list_display_links = ('name',)
+    list_per_page = 10
     search_fields = ('name', 'country')
     list_filter = ('name', 'country')
+    exclude = ('delete_timestamp', 'deleted_at', 'is_deleted', 'is_active')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'body')
+    list_display_links = ('body',)
+    list_per_page = 20
+    search_fields = ('customer', 'body')
+    list_filter = ('product',)
+    raw_id_fields = ('reply',)
+    exclude = ('delete_timestamp', 'deleted_at', 'is_deleted', 'is_active')
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 2
 
 
 @admin.register(Category)
@@ -30,9 +51,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     list_filter = ('name',)
+    inlines = (ProductInline,)
+    exclude = ('delete_timestamp', 'deleted_at', 'is_deleted', 'is_active')
+    raw_id_fields = ('root',)
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
     list_display = ('value', 'type')
     search_fields = ('value',)
+    exclude = ('delete_timestamp', 'deleted_at', 'is_deleted', 'is_active')
