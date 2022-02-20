99c4c4ee83f08d4848f23eb919fc15dd991b8d52
Shayan <shayansadeghi178@gmail.com>
2022-02-20 11:54:32 +0330

    add customized admin panel for user

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Customer, Address
+from django.contrib.auth.admin import UserAdmin
+from core.models import User
+
+UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1','password2')
+
+admin.site.register(User, UserAdmin)
 
 
 class AddressInline(admin.StackedInline):
@@ -9,8 +19,8 @@ class AddressInline(admin.StackedInline):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email',)
-    search_fields = ('username', 'email',)
+    list_display = ('user', 'gender',)
+    search_fields = ('user', 'gender',)
     inlines = [AddressInline]
 
 
