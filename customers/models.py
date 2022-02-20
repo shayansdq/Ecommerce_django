e89d8fcb02bfbad11227d06542767442efadbde9
Shayan <shayansadeghi178@gmail.com>
2022-02-20 12:12:07 +0330

    add str method

--- customers/models.py
+++ customers/models.py
@@ -1,26 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+from core.models import User
 from django.utils.translation import gettext_lazy as _
 from core.models import BaseModel
+from .constants.values import *
 
 
-class Customer(User):
-    """
-        A class used to implement customers
-    """
-    # first_name = models.CharField(max_length=32, verbose_name='First Name')
-    # last_name = models.CharField(max_length=32, verbose_name='Last Name')
-    # email = models.EmailField(verbose_name='Email')
-    phone_number = models.CharField(max_length=32, verbose_name=_('Phone Number'))
-    gender = models.IntegerField(choices=[
-        (1, _('Male')),
-        (2, _('Female')),
-        (3, _('Other')),
-    ],verbose_name=_('Gender'))
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_('User'))
+    gender = models.IntegerField(choices=GENDER_STATUS, verbose_name=_('Gender'))
 
     class Meta:
         verbose_name = _('Customer')
         verbose_name_plural = _('Customers')
+
+    def __str__(self):
+        return f"Customer: '{self.user.phone}', Gender"
 
 
 class Address(BaseModel):
@@ -29,7 +23,7 @@ class Address(BaseModel):
     """
     state = models.CharField(max_length=50, verbose_name=_('State'))
     city = models.CharField(max_length=50, verbose_name=_('City'))
-    zip_code = models.PositiveIntegerField(unique=True,verbose_name=_('Zip Code'))
+    zip_code = models.PositiveIntegerField(unique=True, verbose_name=_('Zip Code'))
     extra_detail = models.TextField(verbose_name=_('Address detail'))
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses',
                                  verbose_name=_('Customer'))
