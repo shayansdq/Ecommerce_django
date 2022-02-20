92828832fdeedc9415ddf9dba3728ac699197825
Shayan <shayansadeghi178@gmail.com>
2022-02-20 11:54:23 +0330

    add customized admin panel for user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from core.models import User
+
+UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+
+
+# admin.site.register(Brand)
+# admin.site.register(User, UserAdmin)
