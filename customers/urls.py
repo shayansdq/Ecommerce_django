5bd8b09c59d66f68c0a38bc89124b0cad14306eb
Shayan <shayansadeghi178@gmail.com>
2022-02-22 14:45:45 +0330

    create logout url

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'customers'
+
 urlpatterns = [
     path('contact/', views.ContactUsView.as_view(), name='contact_view'),
     path('login-register/', views.LoginRegisterView.as_view(), name='register_login_view'),
     path('login/', views.LoginPostView.as_view(), name='do_login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 ]
