8a45c9a1d63421a97f7bc24827b536c285ba0871
Shayan <shayansadeghi178@gmail.com>
2022-02-22 14:35:22 +0330

    refactor: clear User admin customization

--- customers/admin.py
+++ customers/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from .models import Customer, Address
-from django.contrib.auth.admin import UserAdmin
 from core.models import User
-
-UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
-UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
-UserAdmin.ordering = ('phone',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
-UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1','password2')
-
-admin.site.register(User, UserAdmin)
+from .models import Customer, Address
 
 
 class AddressInline(admin.StackedInline):
@@ -21,7 +12,6 @@ class AddressInline(admin.StackedInline):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('user', 'gender',)
     search_fields = ('user', 'gender',)
-    inlines = [AddressInline]
 
 
 @admin.register(Address)
