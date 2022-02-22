cdb246982947ee1e5fc0d3c81108692abfc59486
Shayan <shayansadeghi178@gmail.com>
2022-02-22 14:34:49 +0330

    refactor: add username to fields and prepopulated it with phone

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,13 @@
+from django.contrib.auth.admin import UserAdmin
+from core.models import User
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin
-
-from core.models import User
 
 UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
 UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.list_display = ('username', 'phone', 'last_name', 'email')
 UserAdmin.ordering = ('phone',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
-
-
-# admin.site.register(Brand)
-# admin.site.register(User, UserAdmin)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'username','password')
+UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'username', 'password1', 'password2')
+UserAdmin.prepopulated_fields = {'username': ('phone',)}
+admin.site.register(User, UserAdmin)
