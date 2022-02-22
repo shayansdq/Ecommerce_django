9a6fb6c664526ffa05a9119f2e5879b726b4b802
Shayan <shayansadeghi178@gmail.com>
2022-02-22 14:45:26 +0330

    refactor & update: all of urls for apps

--- Ecommerce_django/urls.py
+++ Ecommerce_django/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = i18n_patterns(path('admin/', admin.site.urls),
+                            path('', include('products.urls',namespace='products')),
+                            path('customers/', include('customers.urls',namespace='customers')),
+                            path('rosetta/', include('rosetta.urls')),
+                            path('api/', include('api.urls',namespace='api')),
+                            ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
