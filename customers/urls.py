9d0a26eec8a1178948489ae8ba5252df454f5453
Shayan <shayansadeghi178@gmail.com>
2022-02-22 17:38:14 +0330

    add urls for reset password

--- customers/urls.py
+++ customers/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('login-register/', views.LoginRegisterView.as_view(), name='register_login_view'),
     path('login/', views.LoginPostView.as_view(), name='do_login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('confirm/complete', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
