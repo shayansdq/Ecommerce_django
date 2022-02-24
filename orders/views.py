2a1245706293406ee66dc5a01fdce8444dd417ec
Shayan <shayansadeghi178@gmail.com>
2022-02-25 00:48:24 +0330

    add create order view class

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.views import View
+
 
 # Create your views here.
+
+class CreateOrderView(View):
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        print(type(request.POST))
