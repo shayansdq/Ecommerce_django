081bd5a52b61ad3529fc8627e275900e83772334
Shayan <shayansadeghi178@gmail.com>
2022-02-25 00:49:08 +0330

    refactor & update: add ProductListView, ProductDetailView models

--- products/views.py
+++ products/views.py
@@ -1,58 +1,65 @@
+import requests
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from rest_framework.response import Response
 from rest_framework import generics
-
+from django.views.generic import DetailView, ListView
 from .forms import PostSearchForm
 from .models import Product, Category
 from .serializers import ProductSerializer
 
 
-# Create your views here.
-#
-# def product_list_api(request):
-#     """
-#     POST: create Product
-#     GET: List Products
-#     :param request:
-#     """
-#
-#     if request.method == 'GET':
-#         product_serializer = ProductSerializer(Product.objects.all(), many=True)
-#         # return JsonResponse({'data': product_serializer.data}, status=200)
-#         return Response(product_serializer.data)
-#     elif request.method == 'POST':
-#         data = request.POST
-#         product_serializer = ProductSerializer(data=data)
-#         if product_serializer.is_valid():
-#             new_product = product_serializer.save()
-#             print(product_serializer.validated_data['category'])
-#             return JsonResponse({'new_product_id': new_product.id}, status=201)
-#         else:
-#             return JsonResponse({'errors': product_serializer.errors}, status=400)
-#     else:
-#         return JsonResponse({}, status=405)
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 3
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        list_products = Product.objects.all()
+        paginator = Paginator(list_products, self.paginate_by)
+        page = self.request.GET.get('page')
+        try:
+            file_exams = paginator.page(page)
+        except PageNotAnInteger:
+            file_exams = paginator.page(1)
+        except EmptyPage:
+            file_exams = paginator.page(paginator.num_pages)
+
+        context['list_exams'] = file_exams
+        return context
+    # def get(self, request, *args, **kwargs):
+    #     products = Product.objects.all()
+    #     context = {
+    #         'products': products,
+    #     }
+    #     return render(request, 'products/index.html', context)
 
 
-# class ProductApiSam(generics.RetrieveUpdateDestroyAPIView):
-#     serializer_class = ProductSerializer
-#     queryset = Product.objects.all()
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
 
-class ProductListView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'products/products_list.html')
-
-
-class CategoryListView(View):
-    form_class = PostSearchForm
-
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
         categories = Category.objects.all()
-        if request.GET.get('search'):
-            categories = categories.filter(name__contains=request.GET['search'])
-        context = {
-            'categories': categories,
-            'search_form':self.form_class,
-        }
-        return render(request, 'products/categories_list.html', context)
+        this_product = self.object
+        related_products = Product.objects.filter(category=this_product.category)
+        related_products = set(list(related_products)) - set([this_product])
+        slider_one_images = this_product.extra_images.all()[:3]
+        slider_two_images = this_product.extra_images.all()[3:6]
+
+        context['categories'] = categories
+        context['slider_one_images'] = slider_one_images
+        context['slider_two_images'] = slider_two_images
+        context['related_products'] = slider_two_images
+
+        return context
+
+
+# class ProductByCategoryListView(View):
+#     def get(self, request, *args, **kwargs):
+#         pass
