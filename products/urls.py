617988fc2df51ce6686c7e2aabdd0c1fbd729919
Shayan <shayansadeghi178@gmail.com>
2022-02-25 00:47:46 +0330

    refactor & update: add product detail url

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from .views import product_list_api, ProductApiSam
-from .views import ProductListView, CategoryListView
+from .views import ProductListView,ProductDetailView
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
-    path('categories/', CategoryListView.as_view(), name='categories_list')
+    # path('categories/', CategoryListView.as_view(), name='categories_list'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail')
 ]
