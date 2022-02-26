f718a73cbee9bd7ecd17a0cf174daf3591942fc6
Shayan <shayansadeghi178@gmail.com>
2022-02-26 17:37:55 +0330

    create about page view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class AboutPageView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'core/about.html')
