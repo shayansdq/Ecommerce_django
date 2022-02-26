ec0fdf6bbc9f50bfe914574c337bd0f58f76564e
Shayan <shayansadeghi178@gmail.com>
2022-02-26 17:35:49 +0330

    add address inline class in customer admin model

--- customers/admin.py
+++ customers/admin.py
@@ -3,7 +3,7 @@ from core.models import User
 from .models import Customer, Address
 
 
-class AddressInline(admin.StackedInline):
+class AddressInline(admin.TabularInline):
     model = Address
     extra = 1
 
@@ -12,6 +12,7 @@ class AddressInline(admin.StackedInline):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('user', 'gender',)
     search_fields = ('user', 'gender',)
+    inlines = (AddressInline,)
 
 
 @admin.register(Address)
