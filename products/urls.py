6a796a71e9f8c09fa063fdffb7352da7deddfa68
Shayan <shayansadeghi178@gmail.com>
2022-02-27 16:23:35 +0330

    change the method of the products' category url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from .views import product_list_api, ProductApiSam
-from .views import ProductListView,ProductDetailView
+from .views import ProductListView,ProductDetailView,ProductByCategoryListView
 
 app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     # path('categories/', CategoryListView.as_view(), name='categories_list'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail')
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<int:pk>', ProductByCategoryListView.as_view(), name='products_category')
 ]
