e11c279d176e9762db17c88770e115f760121cca
Shayan <shayansadeghi178@gmail.com>
2022-02-28 18:08:47 +0330

    change products list url name ro home

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views import ProductListView,ProductDetailView,ProductByCategoryListView
 
 app_name = 'products'
 urlpatterns = [
-    path('', ProductListView.as_view(), name='products_list'),
+    path('', ProductListView.as_view(), name='home'),
     # path('categories/', CategoryListView.as_view(), name='categories_list'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('category/<int:pk>', ProductByCategoryListView.as_view(), name='products_category')
