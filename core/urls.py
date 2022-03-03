5490acb1c4d819b872327b9f05c98c2a17461927
Shayan <shayansadeghi178@gmail.com>
2022-03-03 20:57:36 +0330

    add contact url

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('about/',views.AboutPageView.as_view(),name='about_page')
+    path('about/',views.AboutPageView.as_view(),name='about_page'),
+    path('contact/',views.ContactPageView.as_view(),name='contact_page')
 ]
