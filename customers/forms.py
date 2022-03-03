a347bc3fbea0456485bbe8b67653e47289a5cb94
Shayan <shayansadeghi178@gmail.com>
2022-03-03 20:52:41 +0330

    refactor extra spaces

--- customers/forms.py
+++ customers/forms.py
@@ -63,12 +63,10 @@ class CustomerForm(forms.Form):
     phone = forms.CharField(max_length=100,
                             widget=forms.TextInput(attrs={'placeholder': 'Phone',
                                                           'class': 'form-control'}))
-
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'First name',
                                                                                'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Last name',
                                                                               'class': 'form-control'}))
-
     email = forms.EmailField(max_length=100,
                              widget=forms.TextInput(attrs={'placeholder': 'Email',
                                                            'class': 'form-control'}))
