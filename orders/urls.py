d39b00f475c29e08b55230565f67f7011853ce8a
Shayan <shayansadeghi178@gmail.com>
2022-03-08 01:15:20 +0330

    add new urls

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('send-order/', views.CreateOrderView.as_view(), name='send_order')
+    path('send-order/', views.CreateOrderView.as_view(), name='send_order'),
+    path('shopping_cart/', views.ShoppingCartView.as_view(), name='cart_detail'),
+    path('cart-detail/', views.CartDetailApi.as_view(), name='api_cart_detail'),
+    path('check-offcode/', views.CheckOutCart.as_view(), name='check_off_code'),
+    path('submit-order/', views.SubmitOrder.as_view(), name='submit_order'),
+    path('load-cartitem/', views.LoadCartItemApi.as_view(), name='load_cart_items'),
 ]
