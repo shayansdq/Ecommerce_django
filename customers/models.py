ba489c882031f122679cf62aa604fb2e09b5a398
Shayan <shayansadeghi178@gmail.com>
2022-03-08 01:10:07 +0330

    add a property and str methods to address

--- customers/models.py
+++ customers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from core.models import User
 from django.utils.translation import gettext_lazy as _
 from core.models import BaseModel
+# from products.models import Product
 from .constants.values import *
 
 
@@ -13,7 +14,7 @@ class Customer(models.Model):
         verbose_name = _('Customer')
         verbose_name_plural = _('Customers')
         permissions = [
-            ('view_profile','Can see the profile in site and change his info')
+            ('view_profile', 'Can see the profile in site and change his info')
         ]
 
     def __str__(self):
@@ -36,3 +37,21 @@ class Address(BaseModel):
         unique_together = ["city", "zip_code"]
         verbose_name = _('Address')
         verbose_name_plural = _('Addresses')
+
+    def __str__(self):
+        return f"{self.state} | {self.city} | {self.extra_detail}"
+
+    @property
+    def str(self):
+        return self.__str__()
+
+
+
+
+class WishList(BaseModel):
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, related_name='whishlist',
+                                 verbose_name=_('Customer'))
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE, verbose_name=_('Product'))
+
+    def __str__(self):
+        return f"{self.customer} like {self.product.name}"
