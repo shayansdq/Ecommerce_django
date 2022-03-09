38c7070ee385ec530fe51cf7dd1356c492b8584f
Shayan <shayansadeghi178@gmail.com>
2022-03-09 23:59:26 +0330

    add remove_loaded_items_key method to the views

--- core/views.py
+++ core/views.py
@@ -3,13 +3,16 @@ from django.views import View
 
 
 # Create your views here.
+from orders.models import CartItem
 
 
 class AboutPageView(View):
     def get(self, request, *args, **kwargs):
+        CartItem.remove_loaded_items_key(request)
         return render(request, 'core/about.html')
 
 
 class ContactPageView(View):
     def get(self, request, *args, **kwargs):
+        CartItem.remove_loaded_items_key(request)
         return render(request, 'core/contact.html')
