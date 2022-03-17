552a3747b65f4bb7b09a38baefe2eb01a5c6cb02
Shayan <shayansadeghi178@gmail.com>
2022-03-18 00:09:09 +0330

    delete some extra comments

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+from comments.models import Comment
+
 
 class PostSearchForm(forms.Form):
-    search = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control mr-sm-2",'type':'search"',
-                                                           'placeholder':'Search','aria-label':'Search'}))
+    search = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control mr-sm-2", 'type': 'search"',
+                                                           'placeholder': 'Search', 'aria-label': 'Search'}))
+
+
