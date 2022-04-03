9528e32cf8d5a9d796622755aa0d641fa5f46611
Shayan <shayansadeghi178@gmail.com>
2022-04-03 18:38:43 +0430

    change get text_lazy to gettext

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Max
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from core.models import BaseModel, BaseDiscount
 from django.urls import reverse
 from customers.models import Customer
